a236302a01096dc0574f5ac4725995caa9986a71
Daniel <info@daniel-bird.co.uk>
2023-02-16 00:00:46 +0000

    Style page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,64 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Montserrat, Raleway } from '@next/font/google';
+import styles from './page.module.css';
+import { GithubIcon } from '@/components/Icons/GithubIcon';
+import { LinkedinIcon } from '@/components/Icons/LinkedinIcon';
+import { EmailIcon } from '@/components/Icons/EmailIcon';
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'], weight: '800' });
+const raleway = Raleway({ subsets: ['latin'], weight: ['400', '600'] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+      <div className={styles.container}>
+        <h1 className={`${styles.name} ${montserrat.className}`}>
+          Daniel Bird
+          <span className={`${styles.tagline} ${raleway.className}`}>
+            Frontend specialist balancing craftsmanship and pragmatism to
+            deliver quality software solutions.
+          </span>
+        </h1>
+
+        <p className={styles.description}>
+          As a creative problem solver, I am focused on writing clean code and
+          implementing XP practices to deliver robust, dynamic, and performant
+          websites and web apps. My past experience as a Digital Designer has
+          given me a keen attention to detail and the ability to consult in
+          areas of UX, UI design, and development.
         </p>
-        <div>
+
+        <div className={`${styles.linkContainer} ${raleway.className}`}>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.linkedin.com/in/birddaniel/"
+            className={styles.link}
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            <LinkedinIcon />
+            <h2>Linkedin</h2>
+          </a>
+
+          <a
+            href="https://github.com/Dan-Bird"
+            className={styles.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubIcon />
+            <h2>Github</h2>
+          </a>
+
+          <a
+            href="mailto:info@daniel-bird.co.uk"
+            className={styles.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <EmailIcon />
+            <h2>Email</h2>
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
